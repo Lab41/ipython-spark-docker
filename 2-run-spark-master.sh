90452990990f149e5a81cda0eb8078b46ac5ca9e
kylef-lab41 <kylef@lab41.org>
2015-07-09 12:52:30 -0700

    Pull latest repo and images before running containers

--- 2-run-spark-master.sh
+++ 2-run-spark-master.sh
@@ -8,6 +8,11 @@ source variables_and_helpers.sh
 __image=$__image_master
 
 
+# update repo and images
+git pull origin master && \
+docker pull $__image # alternatively: ./1-build.sh
+
+
 # get host DNS server (for internal resolution)
 __dns=$(dns_detect)
 
