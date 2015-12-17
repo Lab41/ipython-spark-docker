a0d964125469e92490553c8f4f4c2bb453c3cc97
kylef-lab41 <kylef@lab41.org>
2015-12-17 07:25:28 -0800

    Update jupyter-start script to take options from environment variables

--- dockerfiles/mesos-master/lab41/spark-mesos-base/config/jupyter/jupyter-start.sh
+++ dockerfiles/mesos-master/lab41/spark-mesos-base/config/jupyter/jupyter-start.sh
@@ -15,4 +15,4 @@ cp -f /jupyter_notebook_config.py $HOME/.jupyter/
 sudo cp -f /ipython/profile_default/static/base/images/logo.png /usr/local/lib/python2.7/dist-packages/notebook/static/base/images/logo.png
 
 # start notebook
-PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS=notebook $SPARK_HOME/bin/pyspark --master $SPARK_MASTER $SPARK_WORKER_CONFIG
+PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS="${IPYTHON_OPTS}" $SPARK_HOME/bin/pyspark --master $SPARK_MASTER $SPARK_WORKER_CONFIG
